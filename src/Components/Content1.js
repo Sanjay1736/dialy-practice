5599e7f9029c6ac4f28f454e10fbb51677e3fb3f
Sanjay1736 <92837525+Sanjay1736@users.noreply.github.com>
2022-10-22 17:28:56 +0530

    changes

---
+++ src/Components/Content1.js
@@ -0,0 +1,16 @@
+import React from 'react';
+class Contest1 extends React.Component{
+    render() {
+        return (
+            <div>
+              <h1>Declarative</h1>
+             <p> React makes it painless to create interactive UIs. 
+             Design simple views for each state in your application,
+             and React will efficiently update and render just the right components when your data changes.</p>
+             <p>Declarative views make your code more predictable and easier to debu</p> 
+            </div>  
+        )    
+     }
+ }
+
+ export {Contest1}
